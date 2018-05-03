593f3817d43f4f0322e36b7651826b76c14b0aa4
SGA-Liwei-Wang <xLiwei.Wang@sony.com>
2018-05-03 13:31:08 +0800

    201805031331

---
+++ c_plus_plus/tcp_ip_server/cmake-build-debug/CMakeFiles/tcp_ip_server.dir/DependInfo.cmake
@@ -0,0 +1,34 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/root/mydev/myfiles/library/tcp_ip_server/linuxhttp/LinuxSocket.cpp" "/root/mydev/myfiles/library/tcp_ip_server/cmake-build-debug/CMakeFiles/tcp_ip_server.dir/linuxhttp/LinuxSocket.cpp.o"
+  "/root/mydev/myfiles/library/tcp_ip_server/main.cpp" "/root/mydev/myfiles/library/tcp_ip_server/cmake-build-debug/CMakeFiles/tcp_ip_server.dir/main.cpp.o"
+  "/root/mydev/myfiles/library/tcp_ip_server/signal/HandleSignal.cpp" "/root/mydev/myfiles/library/tcp_ip_server/cmake-build-debug/CMakeFiles/tcp_ip_server.dir/signal/HandleSignal.cpp.o"
+  "/root/mydev/myfiles/library/tcp_ip_server/sqlite/SQLiteOpenHelper.cpp" "/root/mydev/myfiles/library/tcp_ip_server/cmake-build-debug/CMakeFiles/tcp_ip_server.dir/sqlite/SQLiteOpenHelper.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../C:/MinGW/include"
+  "../C:/MinGW/mingw32/include"
+  "../C:/MinGW/msys/1.0/include"
+  "../C:/MinGW/msys/1.0/lib/gcc/i686-pc-msys/3.4.4/include"
+  "../C:/MinGW/msys/1.0/lib/gcc/i686-pc-msys/3.4.4/install-tools/include"
+  "../C:/MinGW/share/doc/gcc/include"
+  "../C:/MinGW/share/doc/gdb/include"
+  "../C:/cygwin64/usr/include"
+  "../C:/cygwin64/usr/include/bash"
+  "../D:/ubuntu/head_files/usr/include"
+  "../D:/ubuntu/head_files/usr/usr/lib/gcc/x86_64-linux-gnu/5/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ c_plus_plus/tcp_ip_server/cmake-build-debug/CMakeFiles/tcp_ip_server.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/tcp_ip_server.dir/main.cpp.o"
+  "CMakeFiles/tcp_ip_server.dir/linuxhttp/LinuxSocket.cpp.o"
+  "CMakeFiles/tcp_ip_server.dir/signal/HandleSignal.cpp.o"
+  "CMakeFiles/tcp_ip_server.dir/sqlite/SQLiteOpenHelper.cpp.o"
+  "tcp_ip_server.pdb"
+  "tcp_ip_server"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/tcp_ip_server.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
