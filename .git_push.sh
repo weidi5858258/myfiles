13d8f72cef2dc10bded5fad9fe8bde17a0675ba0
root <root@WEIDI5858258.localdomain>
2018-08-26 23:47:15 +0800

    201808262347

---
+++ .git_push.sh
@@ -0,0 +1,6 @@
+#! /usr/bin
+
+#run command: . .git_push.sh
+current_time=`date "+%Y%m%d%H%M%S"`
+#Don't change
+./.git_push_impl.sh ${current_time}
---
+++ .git_push_impl.sh
@@ -0,0 +1,36 @@
+#! /usr/bin/expect -f
+
+set current_time [lindex $argv 0]
+# send "${current_time}\n"
+set username "weidi5858258@sina.com"
+set password "wangliwei1986"
+#
+spawn git add .
+#
+spawn git commit -m "${current_time}"
+#
+spawn git push
+#
+expect {
+	-re "Username for 'https://github.com'"
+	{
+		send "${username}\r"
+	}
+	-re "Username for"
+	{
+		send "${username}\r"
+	}
+}
+#
+expect {
+	-re -re "Password for 'https://weidi5858258@sina.com@github.com'"
+	{
+		send "${password}\r"
+	}
+	-re "Password for" 
+	{
+		send "${password}\r"
+	}
+}
+
+interact
