c50d17d1f4e74ea57a81304c9e483fc765c7167f
root <root@WEIDI5858258.localdomain>
2018-12-06 00:27:38 +0800

    "20181206002722"

---
+++ c_plus_plus_project/MyStudy/cmake-build-debug-wsl/CMakeFiles/MyStudy.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MyStudy.dir/main.cpp.o"
+  "CMakeFiles/MyStudy.dir/src/B0.cpp.o"
+  "CMakeFiles/MyStudy.dir/src/Test.cpp.o"
+  "CMakeFiles/MyStudy.dir/src/Pointer.cpp.o"
+  "CMakeFiles/MyStudy.dir/src/Study.cpp.o"
+  "CMakeFiles/MyStudy.dir/src/Method.cpp.o"
+  "CMakeFiles/MyStudy.dir/src/function_study.cpp.o"
+  "CMakeFiles/MyStudy.dir/src/GuessNumber.cpp.o"
+  "CMakeFiles/MyStudy.dir/src/HandleAndroidString.cpp.o"
+  "CMakeFiles/MyStudy.dir/src/thread_pool_simple.c.o"
+  "MyStudy.pdb"
+  "MyStudy"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/MyStudy.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
