2896f6ba09dd39b526fc3146a9a4ef986db5d72e
Alexander.Wang <Alexander.Wang@sony.com>
2019-01-10 17:31:28 +0800

    "20190110173113"

--- android/exoplayer.java
+++ android/exoplayer.java
@@ -4,14 +4,14 @@ public class exoplayer{
 
 /***
 library/core/src/main/java/com/google/android/exoplayer2/upstream
-com.google.android.exoplayer2.upstream.DataSpec
 一个视频的相关信息
+com.google.android.exoplayer2.upstream.DataSpec
 com.google.android.exoplayer2.upstream.DataSource
 com.google.android.exoplayer2.upstream.HttpDataSource
 com.google.android.exoplayer2.upstream.FileDataSource
 FileDataSource:
-private long bytesRemaining;
 一个视频还剩下多少长度没有播放
+private long bytesRemaining;
 private @Nullable RandomAccessFile file;
 file = new RandomAccessFile(dataSpec.uri.getPath(), "r");
 file.seek(dataSpec.position);
@@ -26,10 +26,10 @@ try {
 com.google.android.exoplayer2.upstream.Loader
 	public void run():
 		loadable.load();
-		loadable: com.google.android.exoplayer2.upstream.ParsingLoadable
 		只创建过一次
-		loadable: com.google.android.exoplayer2.source.chunk.ContainerMediaChunk
+		loadable: com.google.android.exoplayer2.upstream.ParsingLoadable
 		以后都是创建这个对象进行的操作(不断的在创建对象)
+		loadable: com.google.android.exoplayer2.source.chunk.ContainerMediaChunk
 com.google.android.exoplayer2.source.chunk.ContainerMediaChunk
 	public final void load():
 		dataSource.open(loadDataSpec)
@@ -54,12 +54,29 @@ com.google.android.exoplayer2.source.chunk.ContainerMediaChunk
 
 com.google.android.exoplayer2.extractor.DefaultExtractorInput
 	dataSource: com.google.android.exoplayer2.upstream.StatsDataSource
+com.google.android.exoplayer2.source.chunk.ContainerMediaChunk
+	dataSource: com.google.android.exoplayer2.upstream.StatsDataSource
+com.google.android.exoplayer2.extractor.DefaultExtractorInput
+	dataSource: com.google.android.exoplayer2.upstream.StatsDataSource
 com.google.android.exoplayer2.upstream.StatsDataSource
 	dataSource: com.google.android.exoplayer2.upstream.cache.CacheDataSource
 com.google.android.exoplayer2.upstream.cache.CacheDataSource
 	currentDataSource: com.google.android.exoplayer2.upstream.DefaultDataSource
 com.google.android.exoplayer2.upstream.DefaultDataSource
 	dataSource: com.google.android.exoplayer2.upstream.DefaultHttpDataSource
+
+
+com.google.android.exoplayer2.source.smoothstreaming.SsMediaSource
+	manifestLoader = new Loader("Loader:Manifest");
+	manifestLoader.startLoading(
+            loadable, this, 
+            loadErrorHandlingPolicy.getMinimumLoadableRetryCount(loadable.type));
+com.google.android.exoplayer2.source.chunk.ChunkSampleStream
+	loader = new Loader("Loader:ChunkSampleStream");// 158
+	loader.startLoading(
+            loadable, this, 
+            loadErrorHandlingPolicy.getMinimumLoadableRetryCount(loadable.type));
+
 
 
 
