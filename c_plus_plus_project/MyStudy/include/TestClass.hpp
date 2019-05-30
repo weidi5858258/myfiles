7e7e5685ff8fd33f734a751af02a357b84a79dbc
Alexander <weidi5858258@sina.com>
2019-05-30 21:19:57 +0800

    "20190530211942"

---
+++ c_plus_plus_project/MyStudy/include/TestClass.hpp
@@ -0,0 +1,24 @@
+//
+// Created by root on 19-5-30.
+//
+
+#ifndef MYSTUDY_TESTCLASS_HPP
+#define MYSTUDY_TESTCLASS_HPP
+
+// 爷爷类
+class Grandpa {
+
+};
+
+// 爸爸类
+class Father : public Grandpa {
+
+};
+
+// 儿子类
+class Son : public Father {
+
+};
+
+
+#endif //MYSTUDY_TESTCLASS_HPP
